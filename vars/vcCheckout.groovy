e5a3439d5e8ee11a5cb5e5e471b64e39eb36c0cc
Yurii <55231282+Toto0407@users.noreply.github.com>
2021-08-18 16:01:18 +0300

    new varibles

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -1,22 +1,22 @@
 def call(Map stageParams){
-    def r_dir = new File( stageParams.repodirname )
+    def repo_dir = new File( stageParams.src_repo_name )
     
-    if (r_dir.exists()){
-        deleteDir(dir_name)
+    if (repo_dir.exists()){
+        deleteDir(repo_dir)
     }
-    r_dir.mkdir()
+    repo_dir.mkdir()
     
-    dir("${stageParams.repodirname}"){
+    dir("${stageParams.src_repo_name}"){
     checkout([
         $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
+        branches: [[name:  stageParams.src_repo_branch ]],
+        userRemoteConfigs: [[ url: stageParams.src_repo_url ]]
     ])
-     new AntBuilder().copy( todir: stageParams.destpath ) {
-         fileset( dir:stageParams.srcpath )
+     new AntBuilder().copy( todir: stageParams.dst_repo_dir ) {
+         fileset( dir:stageParams.src_repo_dir )
      }   
      sh"""
-     ls -la ../${stageParams.destpath}
+     ls -la ../${stageParams.dst_repo_di}
      """    
     } 
     
