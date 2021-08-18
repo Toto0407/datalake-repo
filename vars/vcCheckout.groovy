97eccf626656c6d42d42863b0f1d17029115f8bc
Yurii <55231282+Toto0407@users.noreply.github.com>
2021-08-18 16:23:54 +0300

    tetst

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -4,18 +4,11 @@ def call(Map stageParams){
     if (repo_dir.exists()){
         deleteDir(repo_dir)
     }
-    repo_dir.mkdir()
-    
-    dir("${stageParams.src_repo_name}"){
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.src_repo_branch ]],
-        userRemoteConfigs: [[ url: stageParams.src_repo_url ]]
-    ])  
+   
      sh"""
      ls -la
      cp ./${stageParams.src_repo_dir}${stageParams.file_mask} ../${stageParams.dst_repo_dir}
      ls -la ../${stageParams.dst_repo_dir}
      """    
-    }
+ 
 }
