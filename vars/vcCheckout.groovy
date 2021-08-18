d61f2521f282281ddce132c6c53d779f41dc9304
Yurii <55231282+Toto0407@users.noreply.github.com>
2021-08-18 18:00:01 +0300

    34

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -6,7 +6,7 @@ def call(Map stageParams){
         branches: [[name:  stageParams.src_repo_branch ]],
         userRemoteConfigs: [[ url: stageParams.src_repo_url ]]
     ])
-        Files.copy(Paths.get("./${stageParams.src_repo_dir}"), Paths.get("../${stageParams.dst_repo_dir}"), StandardCopyOption.REPLACE_EXISTING)   
+        Files.copy(Paths.get("emr/"), Paths.get("aws/unified/dev/environment/files/"), StandardCopyOption.REPLACE_EXISTING)   
      sh"""
      #cp ./${stageParams.src_repo_dir}${stageParams.file_mask} ../${stageParams.dst_repo_dir}
      ls -la ../${stageParams.dst_repo_dir}
