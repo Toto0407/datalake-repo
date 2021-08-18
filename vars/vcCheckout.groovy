4352e854ca02f564acb301f465f7c66bb89060a6
Yurii <55231282+Toto0407@users.noreply.github.com>
2021-08-18 16:36:01 +0300

    tretst

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -2,7 +2,7 @@ def call(Map stageParams){
     def repo_dir = new File( stageParams.src_repo_name )
     sh'pwd'
     if (repo_dir.exists()){
-        deleteDir(repo_dir)
+        repodir.deleteDir()
     }
    
      sh"""
