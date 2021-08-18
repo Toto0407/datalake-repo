7c8d42f26e0be18d9f2d288136203a1f5ac0781a
Yurii <55231282+Toto0407@users.noreply.github.com>
2021-08-18 18:10:24 +0300

    Update vcCheckout.groovy

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -1,8 +1,4 @@
-@Grab(group = 'commons-io', module = 'commons-io', version = '2.6')
-import org.apache.commons.io.FileUtils
 def call(Map stageParams){
-    def src = new File("/emr/")
-    def dest = new File("/aws/unified/dev/environment/files/")
     sh 'ls -la'
     dir("${stageParams.src_repo_name}"){
     checkout([
@@ -10,13 +6,8 @@ def call(Map stageParams){
         branches: [[name:  stageParams.src_repo_branch ]],
         userRemoteConfigs: [[ url: stageParams.src_repo_url ]]
     ])
-    try {
-    FileUtils.copyDirectory(src, dest);
-      } catch (IOException e) {
-            e.printStackTrace();
-     }
      sh"""
-     #cp ./${stageParams.src_repo_dir}${stageParams.file_mask} ../${stageParams.dst_repo_dir}
+     cp ./${stageParams.src_repo_dir}${stageParams.file_mask} ../${stageParams.dst_repo_dir}
      ls -la ../${stageParams.dst_repo_dir}
      """    
    }
