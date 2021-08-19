51de663962d15178ddbafcdf685f39e4b5a31ca9
Yurii <55231282+Toto0407@users.noreply.github.com>
2021-08-19 16:15:00 +0300

    12

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -1,14 +1,10 @@
 def call(Map stageParams){
-    sh 'ls -la'
     dir("${stageParams.src_repo_name}"){
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.src_repo_branch ]],
         userRemoteConfigs: [[ url: stageParams.src_repo_url ]]
     ])
-     sh"""
-     cp ./${stageParams.src_repo_dir}${stageParams.file_mask} ../${stageParams.dst_repo_dir}
-     ls -la ../${stageParams.dst_repo_dir}
-     """    
+     sh 'cp ./${stageParams.src_repo_dir}${stageParams.file_mask} ../${stageParams.dst_repo_dir}'    
    }
 }
