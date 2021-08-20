91d378d0e4b5ed8468569a9c88e7a4fa4fdd5821
Yurii <55231282+Toto0407@users.noreply.github.com>
2021-08-20 15:39:47 +0300

    454

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -11,18 +11,9 @@ def call(Map stageParams){
         sh"""
         ls -la
         """ 
-        URI uri = null
-         try {
-             uri = new URI("/emr")
-             }
-        Path source=Paths.get(uri)
-         URI uri2 = null
-         try {
-             uri2 = new URI("/aws/unified/dev/environment/files/")  
-             }
-        Path target=Paths.get(uri2)
-        
-        Files.copy(source, target)
+        new AntBuilder().copy(todir: stageParams.dst_repo_dir) {
+        fileset(dir: stageParams.src_repo_dir, includes: "**")
+}
         
         sh"""
         ls -la ../aws/unified/dev/environment/files/
