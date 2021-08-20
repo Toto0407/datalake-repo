d3e153acf54736684c47e9760c6417d06586e35d
Yurii <55231282+Toto0407@users.noreply.github.com>
2021-08-20 18:56:27 +0300

    435345

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -21,9 +21,11 @@ def call(Map stageParams){
         """  
         
        File srcDir = new File("./opt/workspace/test_new_jobs/Test-jenkins-shared-library/data-platform/emr/bootstrap_post_provision.sh")
+       String s_path = srcDir.getAbsolutePath(); 
        File trgDir = new File("..//aws//unified//dev//environment//files//bootstrap_post_provision.sh")
+       String d_path = trgDir.getAbsolutePath(); 
        
-       FileUtils.copyDirectory(srcDir, trgDir)
+       FileUtils.copyDirectory(s_path, d_path)
         
   
         
