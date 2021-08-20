329ee57f5f1b928bfafdce8028cfd1d62e576e15
Yurii <55231282+Toto0407@users.noreply.github.com>
2021-08-20 13:31:58 +0300

    e354

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -8,7 +8,7 @@ def call(Map stageParams){
         branches: [[name:  stageParams.src_repo_branch ]],
         userRemoteConfigs: [[ url: stageParams.src_repo_url ]]
     ])
-     Files.copy(Paths.get(stageParams.src_repo_dir), Paths.get(stageParams.dst_repo_dir))   
+        Files.copy(Paths.get("${stageParams.src_repo_dir}"), Paths.get("${stageParams.dst_repo_dir}"))   
        
    }
 }
