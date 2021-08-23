8365ea3a90eacf05270e05d7f6cc6689f8abb6c2
Yurii <55231282+Toto0407@users.noreply.github.com>
2021-08-23 13:27:53 +0300

    2

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -1,4 +1,5 @@
-import org.apache.tools.ant.BuildEvent
+import groovy.util.AntBuilder 
+import java.io.File 
 def call(Map stageParams){
     dir("${stageParams.src_repo_name}"){
     checkout([
@@ -6,9 +7,12 @@ def call(Map stageParams){
         branches: [[name:  stageParams.src_repo_branch ]],
         userRemoteConfigs: [[ url: stageParams.src_repo_url ]]
     ])
-        new AntBuilder().copy( todir:'/aws/unified/dev/environment/files' ) {
-        fileset( dir:'/emr' )
+        new AntBuilder().copy(todir: "/aws/unified/dev/environment/files") {
+        fileset(dir : "/emr") {
+        include(name:"**/*.*")
+        exclude(name:"**/*Test.java")
         }
+      }
         sh"""
         ls -la ../${stageParams.dst_repo_dir}
         """  
