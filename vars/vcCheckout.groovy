64e80a46e23642702352889a960a5249e944f4b4
Yurii <55231282+Toto0407@users.noreply.github.com>
2021-12-14 13:42:25 +0200

    Update vcCheckout.groovy

--- vars/vcCheckout.groovy
+++ vars/vcCheckout.groovy
@@ -5,6 +5,6 @@ def call(Map stageParams){
         branches: [[name:  stageParams.src_repo_branch ]],
         userRemoteConfigs: [[ url: stageParams.src_repo_url ]]
     ])
-     sh("cp ./${stageParams.src_repo_dir}${stageParams.file_mask} ../${stageParams.dst_repo_dir}")
+     sh("cp -r ./${stageParams.src_repo_dir}${stageParams.file_mask} ../${stageParams.dst_repo_dir}")
    }
 }
